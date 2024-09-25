1dd4a48ee3c86db17bddbb9ddf8d4081ab17c720
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-25 17:31:15 +0530

    added trycatch to commander

--- src/Commander.ts
+++ src/Commander.ts
@@ -1,7 +1,8 @@
 import { Command } from 'commander'
 import { action } from './Actions'
-import { chalkGreen } from './Chalk'
+import { chalkGreen, chalkRed } from './Chalk'
 import { createViteTemplate } from './actions/'
+import { tryCatch } from './utils/helper'
 
 export default class Commader extends Command {
     constructor() {
@@ -9,18 +10,19 @@ export default class Commader extends Command {
     }
 
     async init() {
-        super
-            .version('0.0.1')
-            .description(
-                'CLI to automate react project development with support for additional dependacies'
-            )
-            .action(async () => {
-                const answers = await action()
-                const { appName, language } = answers
+        const [error] = tryCatch(() => {
+            super
+                .version('0.0.1')
+                .description('CLI to automate react project development with support for additional dependacies')
+                .action(async () => {
+                    const answers = await action()
+                    const { appName, language } = answers
 
-                // Create vite template
-                chalkGreen(`Creating app ${appName} with ${language}`)
-                createViteTemplate(appName, language)
-            })
+                    // Create vite template
+                    chalkGreen(`Creating app ${appName} with ${language}`)
+                    createViteTemplate(appName, language)
+                })
+        })
+        if (error) return chalkRed('There was an error in Commander.init()')
     }
 }
