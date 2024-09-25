8484088921fb09457bdb4ea6d7c79fcfec9ad6c4
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-25 18:42:03 +0530

    edit html title and cleared readme

--- src/actions/createViteTemplate.ts
+++ src/actions/createViteTemplate.ts
@@ -1,10 +1,11 @@
 import degit from 'degit'
-import { tryCatch } from '../utils/helper'
-import { chalkRed } from '../Chalk'
+import { editHtmlFileDom, tryCatch, editFile } from '../utils/helper'
+import { chalkGreen, chalkRed } from '../Chalk'
 
 export default async function createViteTemplate(appName: string, language: string) {
     const [error, data] = tryCatch(async () => {
-        const viteTemplate = language === 'TypeScript' ? 'template-react-ts' : 'template-react'
+        const ts = language === 'TypeScript'
+        const viteTemplate = ts ? 'template-react-ts' : 'template-react'
 
         const repo = degit(`vitejs/vite/packages/create-vite/${viteTemplate}`, {
             force: true,
@@ -12,7 +13,18 @@ export default async function createViteTemplate(appName: string, language: stri
         await repo.clone(appName)
         process.chdir(appName)
 
-        const extension = language === 'TypeScript' ? 'ts' : 'js'
+        // edit title in index.html to set it to the app name
+        editHtmlFileDom('index.html', ($) => {
+            $('title').text(appName)
+        })
+
+        // todo: edit package.json to set the app name and description
+
+        // clear readme.md and add my custom readme
+        editFile('README.md', () => readMe())
+        chalkGreen(`README.md updated successfully`)
+
+        const extension = ts ? 'ts' : 'js'
         const jsxExtension = `${extension}x`
 
         return true
@@ -20,3 +32,6 @@ export default async function createViteTemplate(appName: string, language: stri
 
     if (error) return chalkRed(`There was an error in ${arguments.callee.name}, ${error}`)
 }
+
+// my readme.md in markdown language
+const readMe = () => ``
