b6b5454f8a18dc633210ebeaa171a9be103042c8
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-25 14:16:11 +0530

    added prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   { ignores: ["dist/**"] },
-  {files: ["src/**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["src/**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 ];
