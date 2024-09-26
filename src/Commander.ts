716b581aa41112065bf4b496cf02cac8e5bf6324
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-26 19:16:04 +0530

    added boostrap to main commander

--- src/Commander.ts
+++ src/Commander.ts
@@ -1,7 +1,7 @@
 import { Command } from 'commander'
 import { action } from './Actions'
 import { chalkBlue, chalkGreen, chalkRed } from './Chalk'
-import { createViteTemplate } from './actions/'
+import { createViteTemplate, installBootstrap } from './actions/'
 import { tryCatch } from './utils/helper'
 
 export default class Commader extends Command {
@@ -12,23 +12,36 @@ export default class Commader extends Command {
     }
 
     async init() {
-        const [error, done] = tryCatch(() => {
+        const [error, done] = await tryCatch(() => {
             super
                 .version('0.0.1')
                 .description('CLI to automate react project development with support for additional dependacies')
                 .action(async () => {
                     const answers = await action()
-                    const { appName, language } = answers
+                    const { appName, language, cssFramework } = answers
 
                     // Create vite template
                     chalkBlue(`Pulling vite app with name ${appName} in ${language}`)
-                    createViteTemplate(appName, language)
+                    await createViteTemplate(appName, language)
+
+                    if (cssFramework !== 'None') {
+                        switch (cssFramework) {
+                            case 'Tailwind':
+                                break
+
+                            case 'Bootstrap':
+                                await installBootstrap(appName)
+                                break
+                            default:
+                                break
+                        }
+                    }
 
                     this.appName = appName
                 })
             return true
         })
         if (error) return chalkRed('There was an error in Commander.init()')
-        if (done) return chalkGreen(`cd ${this.appName} && npm install`)
+        // if (done) return chalkGreen(`cd ${this.appName} && npm install`)
     }
 }
